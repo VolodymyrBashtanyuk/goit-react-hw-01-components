321c6da3f815d38a31056916f4fb698325c27608
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-09-06 21:55:12 +0300

    add task 01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from 'components/Profile/Profile';
+import user from 'user.json';
+ 
+
+ export const App = () => {
+    return (
+      <div>
+        <Profile user ={user} />
+      </div>
+    );
+  };
+  
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+
+export const Profile = ({ user }) => {
+const {username, tag, location, avatar, stats } = user;
+
+    return <div className="profile">
+    <div className="description">
+      <img
+        src={avatar}
+        alt={username}
+        className="avatar"
+      />
+      <p className="name">{username}</p>
+      <p className="tag">{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+  
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+
+}
+
+Profile.propTypes = {
+    user: PropTypes.shape(
+    {username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number),
+}),
+}
+
+
+
+
+
+
+
+//   <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
