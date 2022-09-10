b6ea0065f1b7002ab7163f45f80157f32878a9a1
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-09-10 14:14:21 +0300

    add correction friendList and profile  propTypes

--- src/components/FriendList/FrendList.jsx
+++ src/components/FriendList/FrendList.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
-import { Friend } from './Friend';
-import { Wrapper } from './FrendListStyled';
+import { Wrapper, Item, Status, Avatar, Name } from './FrendListStyled';
 
 export const FriendList = ({ friends }) => {
-  return (
-    <Wrapper>
-      <Friend friends={friends} />
-    </Wrapper>
-  );
+  const friend = friends.map(({ id, name, avatar, isOnline }) => (
+    <Item key={id}>
+      <Status status={isOnline} />
+      <Avatar src={avatar} alt={name} width="48" />
+      <Name>{name}</Name>
+    </Item>
+  ));
+  return <Wrapper>{friend}</Wrapper>;
 };
 
 FriendList.propTypes = {
-  friends: PropTypes.array.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
 };
